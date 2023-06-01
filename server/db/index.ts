27b75e0b04ac4fbfc01e95631babacfe597d3b7a
syahrulUlum <sahrululum73@gmail.com>
2023-06-02 06:35:13 +0700

    first commit

---
+++ server/db/index.ts
@@ -0,0 +1,17 @@
+import mysql from 'mysql2/promise';
+
+interface Options {
+    query: string;
+    values?: any[];
+}
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'nuxt-ssr'
+});
+
+export const sql = async ({ query, values }: Options) => {
+    const [rows] = await pool.query(query, values);
+    return rows;
+}
---
+++ server/model/blog.ts
@@ -0,0 +1,65 @@
+import { sql } from "../db";
+
+export type BlogModel = {
+    id: number;
+    title: string;
+    content: string;
+    created_date: string;
+}
+
+export const read = async () => {
+    const result = await sql({
+        query: "SELECT * FROM blog"
+    });
+    return result as BlogModel[];
+}
+
+export const create = async (data: Pick<BlogModel, 'title' | 'content'>) => {
+    const result = await sql({
+        query: `
+        INSERT INTO blog (
+            title,
+            content
+            ) VALUES (
+                ?,
+                ?
+            )
+        `,
+        values: [data.title, data.content]
+    }) as any;
+
+    return result.length === 1 ? (result[0] as BlogModel) : null;
+}
+
+export const detail = async (id: string) => {
+    const result = await sql({
+        query: "SELECT * FROM blog WHERE id = ?",
+        values: [id]
+    }) as any;
+    return result.length === 1 ? (result[0] as BlogModel) : null;
+}
+
+
+export const update = async (id: string, data: Pick<BlogModel, 'title' | 'content'>) => {
+    await sql({
+        query: `
+        UPDATE blog
+        SET
+            title = ?,
+            content = ?
+        WHERE id = ?
+        `,
+        values: [data.title, data.content, id]
+    }) as any;
+
+    return await detail(id);
+}
+
+export const remove = async (id: string) => {
+    await sql({
+        query: "DELETE FROM blog WHERE id = ?",
+        values: [id]
+    })
+
+    return true;
+}
